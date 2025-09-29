b7aa04926feff749bafb93f3014db3c112c66c73
baidanchun <2221699889@qq.com>
2025-09-29 19:13:41 +0800

    Update upstash.db.ts

--- src/lib/upstash.db.ts
+++ src/lib/upstash.db.ts
@@ -6,7 +6,7 @@ import { AdminConfig } from './admin.types';
 import { Favorite, IStorage, PlayRecord, SkipConfig } from './types';
 
 // 搜索历史最大条数
-const SEARCH_HISTORY_LIMIT = 20;
+const SEARCH_HISTORY_LIMIT = 50;
 
 // 数据类型转换辅助函数
 function ensureString(value: any): string {
